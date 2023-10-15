09e8ff5416c465d897a29b822e27bfc9516e246d
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2023-10-14 22:48:09 -0600

    clase 3 finalizada

--- Clase3/app.js
+++ Clase3/app.js
@@ -11,8 +11,6 @@ app.disable('x-powered-by');
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'web')));
 
-
-
 app.get('/', (req, res) => {
   res.render('index');
 });
@@ -25,13 +23,13 @@ app.get('/movies', (req, res) => {
 
   if (!genero) return res.json(movies);
 
-  const respuesta = movies.filter(
-    movie => movie.genre.some(g => g.toLowerCase() === genero.toLowerCase())
+  const respuesta = movies.filter(movie =>
+    movie.genre.some(g => g.toLowerCase() === genero.toLowerCase())
   );
 
   if (respuesta.length > 0) return res.json(respuesta);
 
-  res.status(404).json({ "message": "Generos not found" })
+  res.status(404).json({ message: 'Generos not found' });
 
   // else if (genero.length > 1) {
   //   // const respuesta = movies.filter((movie) => movie.genre.forEach(movi => console.log('Gen:', genero, 'Movi:', movi, '--', movi.includes(genero))));
@@ -43,24 +41,22 @@ app.get('/movies', (req, res) => {
 
   //   res.status(404).json({ "message": "Generos not found" })
   // }
-
-})
+});
 
 app.get('/movies/:id', (req, res) => {
-  const { id } = req.params
+  const { id } = req.params;
   const movie = movies.find(movi => movi.id === id);
 
   if (movie) return res.json(movie);
 
-  res.status(404).json({ "message": "Movie not found" })
-})
-
+  res.status(404).json({ message: 'Movie not found' });
+});
 
 app.post('/movies', (req, res) => {
   const result = validateMovie(req.body);
 
   if (result.error) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
   }
 
   // console.log('Data1:', ...result.data);
@@ -68,51 +64,57 @@ app.post('/movies', (req, res) => {
 
   const newMovie = {
     id: crypto.randomUUID(), //uuid v4
-    ...result.data
-  }
+    ...result.data,
+  };
 
   movies.push(newMovie);
   console.log(req.body);
 
-  res.status(201).json({ "message": "Movie Create Corrected" })
-})
+  res.status(201).json({ message: 'Movie Create Corrected' });
+});
 
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params;
+  const movieIndex = movies.findIndex(movie => movie.id === id);
+
+  if (movieIndex === -1) return res.status(404).json({ message: 'Movie not found' });
+
+  movies.splice(movieIndex, 1);
+
+  return res.json({ message: 'Movie deleted' });
+});
 
 app.patch('/movies/:id', (req, res) => {
   const result = validatePartialMovie(req.body);
 
   if (result.error) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
   }
 
   const { id } = req.params;
   console.log(id);
   const { year } = result;
 
-
   const movie = movies.find(movi => movi.id === id);
 
-  if (!movie) return res.status(404).json({ "message": "Movie no fount" })
-
+  if (!movie) return res.status(404).json({ message: 'Movie no fount' });
 
   const updateMovie = {
     ...movie,
-    ...result.data
-  }
+    ...result.data,
+  };
 
-  res.status(201).json({ "message": "Update corected", updateMovie })
-
-})
+  res.status(201).json({ message: 'Update corected', updateMovie });
+});
 
 // Error, La ultima ruta  a la que va a llegar
 app.use((req, res) => {
-  res.status(404).send("<h1>404</h1>");
+  res.status(404).send('<h1>404</h1>');
 });
-
 
 const PORT = process.env.PORT ?? 3000;
 
 //Listen
 app.listen(PORT, () => {
   console.log(`Server listenig on port http://localhost:${PORT}`);
-})
+});
